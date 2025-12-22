ca48481fa06d27c56ab1081896c3746c997db2de
elieEscal67 <elie.ferradji@escal.fr>
2025-12-22 16:41:08 +0100

    first commit for first exercice with some concepts about signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-pokedex-app';
+  
+  name = signal("Pikachu");
+  life = signal(21);
+  size = computed(() => {
+    if(this.life() <= 15) {
+      return "Petit";
+    } else if (this.life() <= 25) {
+      return "Moyen";
+    } else {
+      return "Grand";
+    }
+  })
+
+  
+
+  incrementLife() {
+    this.life.update((life) => life + 1);
+
+  }
+  decrementLife() {
+    this.life.update((life) => life - 1);
+
+  }
+
+  
 }
